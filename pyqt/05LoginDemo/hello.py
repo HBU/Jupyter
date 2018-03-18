6cb2272d9b46e33fbf50c84aaff65f90d6375f3b
David <8584751@qq.com>
2018-03-19 00:05:20 +0800

    消除warn

--- pyqt/05LoginDemo/hello.py
+++ pyqt/05LoginDemo/hello.py
@@ -1,22 +1,39 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+import sys
 
 class hello_mainWindow(QtWidgets.QMainWindow):
-
     def __init__(self):
         super(hello_mainWindow,self).__init__()
-        self.setupUi(self)
-        self.retranslateUi(self)
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle("PushButton")
+        self.setGeometry(400,400,300,260)
 
-    def setupUi(self, mainWindow):
-        mainWindow.setObjectName("mainWindow")
-        mainWindow.setWindowModality(QtCore.Qt.WindowModal)
-        mainWindow.resize(624, 511)
-        self.retranslateUi(mainWindow)
-        QtCore.QMetaObject.connectSlotsByName(mainWindow)
-
-    def retranslateUi(self, mainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        mainWindow.setWindowTitle(_translate("mainWindow", "hello world"))
+        self.closeButton = QPushButton(self)
+        self.closeButton.setText("Close")          #text
+        self.closeButton.setIcon(QIcon("close.png")) #icon
+        self.closeButton.setShortcut('Ctrl+D')  #shortcut key
+        self.closeButton.clicked.connect(self.close)
+        self.closeButton.setToolTip("Close the widget") #Tool tip
+        self.closeButton.move(100,100)
+    # def __init__(self):
+    #     super(hello_mainWindow,self).__init__()
+    #     self.setupUi(self)
+    #     self.retranslateUi(self)
+    #
+    # def setupUi(self, mainWindow):
+    #     mainWindow.setObjectName("DataWindow")
+    #     mainWindow.setWindowModality(QtCore.Qt.WindowModal)
+    #     mainWindow.resize(624, 511)
+    #     self.retranslateUi(mainWindow)
+    #     QtCore.QMetaObject.connectSlotsByName(mainWindow)
+    #
+    # def retranslateUi(self, mainWindow):
+    #     _translate = QtCore.QCoreApplication.translate
+    #     mainWindow.setWindowTitle(_translate("DataWindow", "hello world"))
 
 if __name__ == "__main__":
     import sys
--- pyqt/05LoginDemo/test.py
+++ pyqt/05LoginDemo/test.py
@@ -1,2 +1,29 @@
 sqlstr = 'select * from usertable where userid = '+  '1' +' and password = '
 print(sqlstr)
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+import sys
+
+class PushButton(QWidget):
+    def __init__(self):
+        super(PushButton,self).__init__()
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle("PushButton")
+        self.setGeometry(400,400,300,260)
+
+        self.closeButton = QPushButton(self)
+        self.closeButton.setText("Close")          #text
+        self.closeButton.setIcon(QIcon("close.png")) #icon
+        self.closeButton.setShortcut('Ctrl+D')  #shortcut key
+        self.closeButton.clicked.connect(self.close)
+        self.closeButton.setToolTip("Close the widget") #Tool tip
+        self.closeButton.move(100,100)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = PushButton()
+    ex.show()
+    sys.exit(app.exec_())
