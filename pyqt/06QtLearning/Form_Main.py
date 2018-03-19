cdc81a5cdd473e19d5da106c87ea6c752c40e804
David <8584751@qq.com>
2018-03-19 23:39:40 +0800

    GUI 打开数据库

--- pyqt/06QtLearning/Form_Main.py
+++ pyqt/06QtLearning/Form_Main.py
@@ -1,6 +1,6 @@
 import pymssql
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QHeaderView
 
 from main import Ui_Dialog_Main
 
@@ -9,31 +9,52 @@ class MainDialog(QDialog,Ui_Dialog_Main):
     def __init__(self):
         QDialog.__init__(self)
         self.setupUi(self)
+        result = self.DatabaseQuery()
         # 设置一个4*4的表格数据模型
         self.model = QStandardItemModel(0, 0)
         # 设置横坐标每项的属性名
         self.model.setHorizontalHeaderLabels(['UseID', 'PassWord'])
         # 配置数据，注意！！！需要使用QStandardItem格式的文本
-        Data = self.DatabaseQuery()
-        print(str(Data))
         i = 0
-        for r in Data:
+        print('tableShow:'+str(result))
+        for r in result:
             self.model.setItem(i, 0, QStandardItem(str(r[0])))
             self.model.setItem(i, 1, QStandardItem(str(r[1])))
             i += 1
         self.tableView.setModel(self.model)
+        # 下面代码让表格100%填满窗口
+        self.tableView.horizontalHeader().setStretchLastSection(True)
+        self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
     def btnInsert(self):
         pass
+
     def btnDelete(self):
         pass
+
     def btnUpdate(self):
         pass
+
     def btnQuery(self):
-        pass
+        conn = pymssql.connect(host='.', user='sa', password='sql', database='Test', charset="GBK")
+        uid = self.lineEdit.text()
+        print(str(uid))
+        sqlstr = 'select * from usertable where userid = \''+ str(uid) +'\''
+        print(sqlstr)
+        try:
+            cur = conn.cursor()
+            cur.execute(sqlstr)
+            result = cur.fetchall()
+        except:
+            print("Error: unable to fetch data")
+        print(str(result)) # 查到数据，但是无法更新到qTableView 。。。
+        cur.close()
+        conn.close()
+
     def btnClose(self):
         self.close()
 
-    def DatabaseQuery(self): # 在数据库中查询
+    def DatabaseQuery(self): # 在数据库中查询全部数据
         print("start DatabaseQuery:")
         conn = pymssql.connect(host='.', user='sa', password='sql', database='Test', charset="GBK")
         print("Database connected!")
